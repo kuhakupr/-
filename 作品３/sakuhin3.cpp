a52eb6b0a8f5a4e56c1029f580dcd352c1074d8a
FBK_FOXShark <hj-g19023@sist.ac.jp>
2020-12-14 12:30:35 +0900

    文字の描画

--- 作品３/sakuhin3.cpp
+++ 作品３/sakuhin3.cpp
@@ -2,6 +2,7 @@
 //########## ヘッダーファイル読み込み ##########
 #include "DxLib.h"
 #include "resouce.h"
+#include  <math.h>
 
 //########## マクロ定義 ##########
 #define GAME_WIDTH			800	//画面の横の大きさ
@@ -23,6 +24,13 @@
 //フォント
 #define FONT_TANU_PATH			TEXT(".\\FONT\\TanukiMagic.ttf")	//フォントの場所
 #define FONT_TANU_NAME			TEXT("たぬき油性マジック")			//フォントの名前
+
+// 文字のサイズ
+#define MOJI_SIZE 24
+
+// 仮想テキストバッファの横サイズ縦サイズ
+#define STRBUF_WIDTH	24
+#define STRBUF_HEIGHT	20
 
 //エラーメッセージ
 #define FONT_INSTALL_ERR_TITLE	TEXT("フォントインストールエラー")
@@ -255,6 +263,25 @@ IMAGE_BLINK ImageEndFAIL;				//エンドフォールの画像
 
 CHARA player;		//ゲームのキャラ
 
+char StringBuf[STRBUF_HEIGHT][STRBUF_WIDTH * 2 + 1];	// 仮想テキストバッファ
+int CursorX, CursorY;						// 仮想画面上での文字表示カーソルの位置
+int SP, CP;							// 参照する文字列番号と文字列中の文字ポインタ
+int EndFlag;							// 終了フラグ
+int KeyWaitFlag;						// ボタン押し待ちフラグ
+int Count;							// フレームカウンタ
+
+char String[][256] =
+{
+	"　ゲームプログラムを習得するための一番の近道はとにかく沢山プログラムを組む",
+	"ことである。B" ,
+	"@　プログラムの参考書にはゲームのプログラムの方法なんて何も書かれていない、B",
+	"変数、B配列、B関数、Bループ、B条件分岐…Bこれらすべての説明はゲームで何に使うか",
+	"なんてどこにも書いていない、Bせいぜい住所録を題材にした例がある程度である。B" ,
+	"C　プログラムは習うより慣れろなのでプログラムを組むに当たって少しでも知識が",
+	"つけば後はそこからは掘り下げ、広げていけば良いわけで、Bプログラムの参考書を",
+	"読んでいて少しでも何か出来るような気がしたらそこでとにかくプログラム",
+	"を打ってみることが大事である。E",
+};
 
 //音楽関連
 MUSIC BGM;			//ゲームのBGM
@@ -265,15 +292,15 @@ MUSIC BGM_FAIL;		//フォールトのBGM
 
 GAME_MAP_KIND mapData[GAME_MAP_TATE_MAX][GAME_MAP_YOKO_MAX]{
 	//  0,1,2,3,4,5,6,7,8,9,0,1,2,
-		k,k,k,k,k,k,k,k,k,k,k,g,k,	// 0
-		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 1
+		k,k,k,k,k,k,k,k,k,k,k,k,k,	// 0
+		s,t,t,t,t,t,t,t,t,t,t,t,k,	// 1
 		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 2
 		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 3
 		k,t,k,k,k,k,k,k,k,k,t,t,k,	// 4
 		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 5
 		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 6
-		k,t,t,t,t,t,t,t,t,t,t,t,k,	// 7
-		k,k,k,k,k,k,k,k,k,k,k,s,k,	// 8
+		k,t,t,t,t,t,t,t,t,t,t,t,g,	// 7
+		k,k,k,k,k,k,k,k,k,k,k,k,k,	// 8
 };	//ゲームのマップ
 
 //ゲームマップの初期化
@@ -336,6 +363,9 @@ VOID MY_DELETE_MUSIC(VOID);		//音楽をまとめて削除する関数
 BOOL MY_CHECK_MAP1_PLAYER_COLL(RECT);	//マップとプレイヤーの当たり判定をする関数
 BOOL MY_CHECK_RECT_COLL(RECT, RECT);	//領域の当たり判定をする関数
 
+void Kaigyou(void);		// テキストバッファの改行処理関数
+
+
 //########## プログラムで最初に実行される関数 ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -364,6 +394,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetMouseDispFlag(TRUE);			//マウスカーソルを表示
 
 	GameScene = GAME_SCENE_START;	//ゲームシーンはスタート画面から
+
+
+	
+
+
+	// 描画位置の初期位置セット
+	CursorX = 0;
+	CursorY = 0;
+
+	// 参照文字位置をセット
+	SP = 0;	// １行目の
+	CP = 0;	// ０文字
+
+
+	// フォントのサイズセット
+	SetFontSize(MOJI_SIZE);
+
+	// フレームカウンタ初期化
+	Count = 0;
 
 	SetDrawScreen(DX_SCREEN_BACK);	//Draw系関数は裏画面に描画
 
@@ -808,12 +857,142 @@ VOID MY_PLAY_PROC(VOID)
 
 		GameScene = GAME_SCENE_IBENTO;
 	}
+
 }
 
 VOID MY_PLAY_DRAW(VOID)
 {
-	DrawGraph(Image.x, Image.y, Image.handle, TRUE);
-	DrawGraph(karikyara.x, karikyara.y, karikyara.handle, TRUE);
+
+	char OneMojiBuf[3];	// １文字分一時記憶配列
+	int i, j;
+
+	// ループ
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_1) == 0)
+	{
+
+
+		// サウンドノベル風文字列描画処理を行う
+		// ただし終了フラグが１だった場合は処理をしない
+		if (EndFlag == 0)
+		{
+			char  Moji;
+
+			// ボタン押し待ちフラグがたっていた場合はボタンが押されるまでここで終了
+			if (KeyWaitFlag == 1)
+			{
+				if (ProcessMessage() == 0 && CheckHitKeyAll() != 0)
+				{
+					// ボタンが押されていたら解除
+					KeyWaitFlag = 0;
+				}
+			}
+			else
+			{
+				// 文字の描画
+				Moji = String[SP][CP];
+				switch (Moji)
+				{
+				case '@':	// 改行文字
+
+					// 改行処理および参照文字位置を一つ進める
+					Kaigyou();
+					CP++;
+
+					break;
+
+				case 'B':	// ボタン押し待ち文字
+
+					// ボタンが離されるまで待つ
+					while (ProcessMessage() == 0 && CheckHitKeyAll() != 0) {}
+
+					// ボタン押し待ちフラグをたてる
+					KeyWaitFlag = 1;
+					CP++;
+
+					{/*
+						int Color;
+
+						Color = (int)(sin(Count / 100.0) * 80.0 + 125.0);
+						DrawBox(0, 0, 0, 0, GetColor(Color, 30, 30), TRUE);*/
+						Count++;
+					}
+					break;
+
+				case 'E':	// 終了文字
+
+					// 終了フラグを立てるおよび参照文字位置を一つ進める
+					EndFlag = 1;
+					CP++;
+					
+					break;
+
+				case 'C':	// クリア文字
+
+					// 仮想テキストバッファを初期化して描画文字位置を初期位置に戻すおよび参照文字位置を一つ進める
+					for (i = 0; i < STRBUF_HEIGHT; i++)
+					{
+						for (j = 0; j < STRBUF_WIDTH * 2; j++)
+						{
+							StringBuf[i][j] = 0;
+						}
+					}
+
+					CursorY = 0;
+					CursorX = 0;
+					CP++;
+
+					break;
+
+				default:	// その他の文字
+
+					// １文字分抜き出す
+					OneMojiBuf[0] = String[SP][CP];
+					OneMojiBuf[1] = String[SP][CP + 1];
+					OneMojiBuf[2] = '\0';
+
+					// １文字テキストバッファに代入
+					StringBuf[CursorY][CursorX * 2] = OneMojiBuf[0];
+					StringBuf[CursorY][CursorX * 2 + 1] = OneMojiBuf[1];
+
+					// 参照文字位置を２バイト進める
+					CP += 2;
+
+					// カーソルを一文字分進める
+					CursorX++;
+
+					// テキストバッファ横幅からはみ出たら改行する
+					if (CursorX >= STRBUF_WIDTH) Kaigyou();
+
+					break;
+				}
+
+				// 参照文字列の終端まで行っていたら参照文字列を進める
+				if (String[SP][CP] == '\0')
+				{
+					SP++;
+					CP = 0;
+				}
+			}
+		}
+
+		// 画面のクリア
+		ClearDrawScreen();
+
+         DrawGraph(Image.x, Image.y, Image.handle, TRUE);
+        DrawGraph(karikyara.x, karikyara.y, karikyara.handle, TRUE);
+   
+		// テキストバッファの描画
+		for (i = 0; i < STRBUF_HEIGHT; i++)
+		{
+			DrawString(8, i * MOJI_SIZE, StringBuf[i], GetColor(0, 0, 0));
+			
+		}
+		
+
+		// 裏画面の内容を表画面に反映させる
+		ScreenFlip();
+	}
+
 
 }
 
@@ -822,7 +1001,6 @@ VOID MY_IBENTO(VOID)
 {
 	MY_IBENTO_PROC();	//プレイ画面の処理
 	MY_IBENTO_DRAW();	//プレイ画面の描画
-
 	return;
 }
 
@@ -1514,4 +1692,31 @@ BOOL MY_CHECK_RECT_COLL(RECT a, RECT b)
 	}
 
 	return FALSE;		//当たっていない
+}
+
+// 改行関数
+void Kaigyou(void)
+{
+	// 描画行位置を一つ下げる
+	CursorY++;
+
+	// 描画列を最初に戻す
+	CursorX = 0;
+
+	// もしテキストバッファ縦幅からはみ出るならテキストバッファを縦スクロールさせる
+	if (CursorY >= STRBUF_HEIGHT)
+	{
+		int i, j;
+
+		for (i = 1; i < STRBUF_HEIGHT; i++)
+		{
+			for (j = 0; j < STRBUF_WIDTH * 2; j++)
+			{
+				StringBuf[i - 1][j] = StringBuf[i][j];
+			}
+		}
+
+		// 描画行位置を一つあげる
+		CursorY--;
+	}
 }
